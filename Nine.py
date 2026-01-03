101c091b976c7a05d18f47da1405d17d42dee359
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2026-01-03 14:55:12 +0530

    Delete Nine.py

--- Nine.py
+++
@@ -1,6 +0,0 @@
-# Question.9- Write a program to take the user's name and age and display: 
-# Hello <name>, you will turn <age+1> next year.
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-next_age = age + 1
-print(f"Hello {name}, you will turn {next_age} next year.") 
