9c1f803c32237375f2a838a0faaef945fdaca1a0
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2026-01-04 14:43:31 +0530

    Create First.py

---
+++ Assignment3/First.py
@@ -0,0 +1,10 @@
+#Question-1: What is user defined functions.
+
+# Write a function to accept two numbers as parameters and return their multiplication.
+
+#Ans: User-defined functions are functions that are created by the user to perform specific tasks. They allow for code reusability and better organization of code. 
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+result = multiply_numbers(5, 10)
+print("The multiplication of the two numbers is:", result)
