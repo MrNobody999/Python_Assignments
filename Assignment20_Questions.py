ea0cbf9586ad65d20ee7741d3ada419a566fb811
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2026-01-27 18:34:10 +0530

    Add files via upload

---
+++ Assignment20_Questions.py
@@ -0,0 +1,44 @@
+
+# 1: Design a Python application that creates two separate threads named Even and Odd.
+# The Even thread should display the first 10 even numbers.
+# The Odd thread should display the first 10 odd numbers.
+# Both threads should execute independently using the threading module.
+# Ensure proper thread creation and execution,
+
+# 2: Design a Python application that creates two threads named EvenFactor and OddFactor.
+# Both threads should accept one integer number as a parameter,
+# The EvenFactor thread Should:
+     # Identify all even factors of the given number.
+     # Calculate and display the sum of even factors.
+# The OddFactor thread should :
+     # Identify all odd factors of the given number.
+     # Calculate and display the sum of odd factors.
+
+# After both threads complete execution, the main thread should display the message:
+# "Exit from main"
+
+# 3: Design a Python application that creates two threads named EvenList and OddList.
+# Both threads should accept a list of integers as input.
+# The EvenList thread should:
+   # Extract all even elements from the list.
+   # Calculate and display their sum,
+# The OddList thread should:
+   # Extract all odd elements from the list.
+   # Calculate and display their sum,
+# Threads should run concurrently
+
+# 4: Design a Python application that creates three threads named Small, Capital, and Digits.
+# All threads should accept a string as input.
+# The Small thread should count and display the number of lowercase characters.
+# The Capital thread should count and display the number of uppercase characters.
+# The Digits thread should count and display the number of numeric digits.
+# Each thread must also display
+   # Thread ID
+   # Thread Name
+
+# 5: Design a Python application that creates two threads named Thread1 and Thread2.
+# Thread1 should display numbers from 1 to 50.
+# Thredd2 should display numbers from 50 to 1 reverse order.
+# Ensure that
+   # Thread2 starts execution only after Thread1 has completed.
+# Use appropriate thread synchronization.
